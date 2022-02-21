ab207f5c7e2b7cc75f4cb8f7f34489401753bce8
Martin Dubois <martindubois1602@gmail.com>
2022-02-21 18:40:56 +0100

    Création controlleur Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
