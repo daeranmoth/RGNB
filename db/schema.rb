53132dca13c8c7765642e91e365fcd29aa5bfffc
Celine Condoris <celine.condoris@gmail.com>
2022-02-21 17:38:28 +0100

    fixing migratin problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_160552) do
-
+ActiveRecord::Schema.define(version: 2022_02_21_155631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
