1a20c143943e9218dea25d6047680dc1c0b993b8
Martin Dubois <martindubois1602@gmail.com>
2022-02-22 11:22:58 +0100

    new.html.erb 1st try

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,31 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     @user.save
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  # end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :city, :gender, :age, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources: :users, only: [ :new, :create, :show ]
+  resources :users, only: [ :new, :create, :show, :destroy ]
 end
