06283268141d2027ab505237dcdcc27ab1070128
Martin Dubois <martindubois1602@gmail.com>
2022-02-22 17:41:20 +0100

    Débug

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :bookings, only: [ :index, :show, :new, :create, :edit, :update ]
-
+  resources :users, only: :show
   resources :experiences
 
 end
