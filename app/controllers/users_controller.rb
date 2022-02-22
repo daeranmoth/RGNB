2378a3264344bcd04a0984d4fbb07e966efa78f3
Martin Dubois <martindubois1602@gmail.com>
2022-02-22 12:01:59 +0100

    New and edit user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,7 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.save
-  end
-
   def show
     @user = User.find(params[:id])
-  end
-
-  # def destroy
-  #   @user = User.find(params[:id])
-  #   @user.destroy
-  # end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
   end
 
   private
